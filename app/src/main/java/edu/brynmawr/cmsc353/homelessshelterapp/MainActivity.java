3634bb99593ff63849715f6d1a7086c92cbb2daf
haiqa-k <79232961+haiqa-k@users.noreply.github.com>
2021-04-24 12:24:40 -0400

    Initial commit

---
+++ app/src/main/java/edu/brynmawr/cmsc353/homelessshelterapp/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.brynmawr.cmsc353.homelessshelterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
